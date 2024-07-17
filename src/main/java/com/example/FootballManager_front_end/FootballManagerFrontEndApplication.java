7d4d1638023ed06b19bfe71ced381716481a8779
TodorYanakiev <todoryanakiev395@gmail.com>
2024-07-17 14:18:23 +0300

    initial commit

---
+++ src/main/java/com/example/FootballManager_front_end/FootballManagerFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.FootballManager_front_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballManagerFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballManagerFrontEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FootballManager_front_end/FootballManagerFrontEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FootballManager_front_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballManagerFrontEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
